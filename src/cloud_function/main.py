e0366eeb741926bdb7eefa6aeb672e06fdc7ef05
Bellanov Apilli <BellanovApilli@gmail.com>
2022-09-25 13:11:49 -0400

    ISSUE-1: Initial Cloud Function Template

---
+++ src/cloud_function/main.py
@@ -0,0 +1,127 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+# [START functions_helloworld_http]
+# [START functions_http_content]
+from flask import escape
+# [START functions_http_method]
+# [START functions_helloworld_get]
+import functions_framework
+
+# [END functions_helloworld_http]
+# [END functions_http_content]
+# [END functions_http_method]
+# [END functions_helloworld_get]
+
+
+# [START functions_helloworld_get]
+@functions_framework.http
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Note:
+        For more information on how Flask integrates with Cloud
+        Functions, see the `Writing HTTP functions` page.
+        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+    """
+    return 'Hello World!'
+# [END functions_helloworld_get]
+
+
+# [START functions_helloworld_http]
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
+# [END functions_helloworld_http]
+
+
+# [START functions_http_content]
+@functions_framework.http
+def hello_content(request):
+    """ Responds to an HTTP request using data from the request body parsed
+    according to the "content-type" header.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    content_type = request.headers['content-type']
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'name' in request_json:
+            name = request_json['name']
+        else:
+            raise ValueError("JSON is invalid, or missing a 'name' property")
+    elif content_type == 'application/octet-stream':
+        name = request.data
+    elif content_type == 'text/plain':
+        name = request.data
+    elif content_type == 'application/x-www-form-urlencoded':
+        name = request.form.get('name')
+    else:
+        raise ValueError("Unknown content type: {}".format(content_type))
+    return 'Hello {}!'.format(escape(name))
+# [END functions_http_content]
+
+
+# [START functions_http_method]
+@functions_framework.http
+def hello_method(request):
+    """ Responds to a GET request with "Hello world!". Forbids a PUT request.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    from flask import abort
+
+    if request.method == 'GET':
+        return 'Hello World!'
+    elif request.method == 'PUT':
+        return abort(403)
+    else:
+        return abort(405)
+# [END functions_http_method]
